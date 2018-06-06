dfc035ab8f5970a462a717e0a7e9dfcfb66fb6b2
nmietus <nmietus93@gmail.com>
2018-06-06 22:23:20 +0200

    Pierwszy projekt springboota

---
+++ springboot-project-1/src/main/java/com/springboot/basics/springbootproject1/SpringbootProject1Application.java
@@ -0,0 +1,12 @@
+package com.springboot.basics.springbootproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProject1Application.class, args);
+	}
+}
